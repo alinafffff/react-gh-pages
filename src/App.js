af3029f2a17b0a5e21ab29f47c5f547b31644b80
alinafffff <alfasar1@yandex.ru>
2024-03-29 11:39:24 +0400

    HELLO WORLD5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HELLO WORLD PLEASE PLEASE!!!!!
+         HELLO WORLD PLEASE PLEASE!!!!!!
         </p>
         <a
           className="App-link"
